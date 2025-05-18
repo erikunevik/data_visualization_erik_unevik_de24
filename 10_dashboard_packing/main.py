bd00f1ccb52f4639389bb94176e9d756fdcba626
Erik Unevik <erikunevik@gmail.com>
2025-05-18 19:52:53 +0200

    2025-05-18

---
+++ 10_dashboard_packing/main.py
@@ -0,0 +1,12 @@
+from taipy.gui import Gui
+from frontend.pages.dashboard import dashboard_page
+from frontend.pages.data import data_page
+from frontend.pages.home import home_page
+
+
+pages = {"home": home_page, "dashboard": dashboard_page, "data": data_page}
+
+if __name__ == "__main__":
+    Gui(pages=pages, css_file="assets/main.css").run(
+        dark_mode=False, use_reloader=True, port=8080
+    )
